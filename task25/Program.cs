b87171e636d3683644c61dd73cd3450f94e4c861
alexandrp9001 <140640940+alexandrp9001@users.noreply.github.com>
2023-09-01 13:03:34 +0300

    task 25

---
+++ task25/Program.cs
@@ -0,0 +1,20 @@
+﻿//Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+Console.Write("Введите число A:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B:");
+int FirstB = Convert.ToInt32(Console.ReadLine());
+int b = FirstB;
+int c = 1;
+if (b<1)
+{
+    Console.Write("Квадрат должен быть натуральным");
+}
+else
+{
+    while (b!=0)
+    {
+        c *= a;
+        b -= 1;
+    }
+    Console.Write($"Число {a} в степени {FirstB} равно {c}");
+}
